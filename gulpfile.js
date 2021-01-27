1578eb115ccfd98577ced146e2c9f0d56301b942
Tomek <tomasz.urbankiewicz@gmail.com>
2021-01-27 20:59:04 +0100

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+const entryPath = "C:/Users/48722/Desktop/materiały z kursu CodersLab/mój projekt/BestShop-JavaScript";
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+sass.compiler = require('sass');
+const sourcemaps = require('gulp-sourcemaps');
+const autoprefixer = require('gulp-autoprefixer');
+const browserSync = require("browser-sync").create();
+function compileSass(done) {
+    gulp.src(entryPath + '/scss/main.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({outputStyle: "expanded"})
+            .on('error', sass.logError))
+        .pipe(autoprefixer())
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest(entryPath + '/css'));
+    done();
+}
+function watcher(done) {
+    browserSync.init({
+        server: entryPath
+    });
+    gulp.watch(entryPath + '/scss/**/*.scss', gulp.series(compileSass, reload));
+    gulp.watch(entryPath + '/*.html', gulp.series(reload));
+}
+function reload(done) {
+    browserSync.reload();
+    done();
+}
+exports.sass = gulp.parallel(compileSass);
+exports.default = gulp.parallel(compileSass, watcher);
